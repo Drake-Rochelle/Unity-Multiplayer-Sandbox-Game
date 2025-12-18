0ddec527e53141e76f92c9425c128ee83a4dec45
Drake-Rochelle <drakerochelle.3ds@gmail.com>
2025-12-18 11:12:38 -0700

    packages1
    packages1

---
+++ Assets/Mini First Person Controller/Scripts/Components/Jump.cs
@@ -0,0 +1,41 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rigidbodyCompnent;
+    public float jumpStrength = 2;
+    public event System.Action Jumped;
+
+    [SerializeField, Tooltip("Prevents jumping when the transform is in mid-air.")]
+    GroundCheck groundCheck;
+    PhotonView view;
+
+
+
+        void Reset()
+    {
+        // Try to get groundCheck.
+        groundCheck = GetComponentInChildren<GroundCheck>();
+    }
+
+    void Awake()
+    {
+        view = GetComponent<PhotonView>();
+        // Get rigidbody.
+        rigidbodyCompnent = GetComponent<Rigidbody>();
+    }
+
+    void LateUpdate()
+    {
+        if (view.IsMine)
+        {
+            // Jump when the Jump button is pressed and we are on the ground.
+            if (Input.GetButtonDown("Jump") && (!groundCheck || groundCheck.isGrounded))
+            {
+                rigidbodyCompnent.AddForce(Vector3.up * 100 * jumpStrength);
+                Jumped?.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
@@ -0,0 +1,50 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class FirstPersonLook : MonoBehaviour
+{
+    [SerializeField]
+    Transform character;
+    public float sensitivity = 2;
+    public float smoothing = 1.5f;
+
+    Vector2 velocity;
+    Vector2 frameVelocity;
+    public void SetCam(Vector2 cam)
+    {
+        velocity.y = -cam.x;
+        velocity.x = cam.y;
+    }
+
+    private void Awake()
+    {
+        if (!GetComponent<PhotonView>().IsMine)
+        {
+            GetComponent<Camera>().enabled = false;
+            GetComponent<AudioListener>().enabled = false;
+        }
+    }
+
+    void Reset()
+    {
+        // Get the character from the FirstPersonMovement in parents.
+        character = GetComponentInParent<FirstPersonMovement>().transform;
+    }
+
+    void Update()
+    {
+        if (Time.timeScale != 0)
+        {
+
+            // Get smooth velocity.
+            Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+            Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
+            frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
+            velocity += frameVelocity;
+            velocity.y = Mathf.Clamp(velocity.y, -90, 90);
+
+            transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
+            character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+        }
+    }
+}
